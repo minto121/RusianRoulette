32760dc8bea971c0017b9e1278444fbd3926c352
L4ntaarn <sho-nagamine-k22h@stu.kbc.ac.jp>
2024-05-15 12:04:48 +0900

    EndSceneのヘッダーとcppファイルの作成
    m'sの気持ちのところで文字化けしてる

---
+++ RousianRoulette/EndScene.cpp
@@ -0,0 +1,40 @@
+#include "DxLib.h"
+#include "Padinput.h"
+#include "Title.h"
+#include "EndScene.h"
+
+EndScene::EndScene()
+{
+
+}
+
+EndScene::~EndScene()
+{
+
+}
+
+AbstractScene* EndScene::Update()
+{
+	static int time = 300;
+	if (time-- <= 0) {
+		return nullptr;
+	}
+	return this;
+}
+
+void EndScene::Draw() const
+{
+	DrawGraph(0, 0, Endimage, FALSE);
+	SetFontSize(50);
+	//DrawString(330, 50, "END", 0x000000);
+	DrawString(100, 50, "Thank you ", 0xffffff);
+	DrawString(260, 100, "for playing!!", 0xffffff);
+	SetFontSize(48);
+	DrawString(100, 420, "m'sの気持ち", 0xffffff);
+	SetFontSize(24);
+	DrawString(100, 480, "matsuda tomohide", 0xffffff);
+	DrawString(100, 510, "matsutake taketo", 0xffffff);
+	DrawString(100, 540, "miyagumi rikuto", 0xffffff);
+	DrawString(100, 570, "nagamine sho", 0xffffff);
+
+}
